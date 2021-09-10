14bd41fdc6b3fdd198ef7955622bb69c628b3b66
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-10 10:36:12 +0100

    used test.TempDir for easier temp dir creation + cleanup

--- filestore_test.go
+++ filestore_test.go
@@ -1,19 +1,11 @@
 package keystore
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestFileStore(t *testing.T) {
-	dir, err := ioutil.TempDir("", "fileStore-test")
-	if err != nil {
-		t.Errorf("received unexpected error creating temp dir: %s", err)
-		return
-	}
-	defer os.RemoveAll(dir)
-	s, err := NewFileStore(dir, "", nil)
+	s, err := NewFileStore(t.TempDir(), "", nil)
 	if err != nil {
 		t.Errorf("received unexpected error creating FileStore: %s", err)
 		return
@@ -22,19 +14,7 @@ func TestFileStore(t *testing.T) {
 }
 
 func TestFileStoreWithTmp(t *testing.T) {
-	dir, err := ioutil.TempDir("", "fileStore-test")
-	if err != nil {
-		t.Errorf("received unexpected error creating temp dir: %s", err)
-		return
-	}
-	defer os.RemoveAll(dir)
-	tmp, err := ioutil.TempDir("", "fileStore-test-tmp")
-	if err != nil {
-		t.Errorf("received unexpected error creating tmp dir: %s", err)
-		return
-	}
-	defer os.RemoveAll(tmp)
-	s, err := NewFileStore(dir, tmp, nil)
+	s, err := NewFileStore(t.TempDir(), t.TempDir(), nil)
 	if err != nil {
 		t.Errorf("received unexpected error creating FileStore: %s", err)
 		return
