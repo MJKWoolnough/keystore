291f3ca680d353027e167d3b3c3c43f8200c5b51
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-16 20:13:38 +0100

    regen'd

--- types.go
+++ types.go
@@ -4,66 +4,8 @@ package keystore
 
 import (
 	"io"
-	"sync"
 
 	"vimagination.zapto.org/byteio"
-)
-
-type readerPool struct {
-	pool sync.Pool
-}
-
-func (rp *readerPool) Get(r io.Reader) *byteio.StickyLittleEndianReader {
-	lr := rp.pool.Get().(*byteio.StickyLittleEndianReader)
-	lr.Reader = r
-
-	return lr
-}
-
-func (rp *readerPool) Put(lr *byteio.StickyLittleEndianReader) (int64, error) {
-	c, err := lr.Count, lr.Err
-	*lr = byteio.StickyLittleEndianReader{}
-
-	rp.pool.Put(lr)
-
-	return c, err
-}
-
-type writerPool struct {
-	pool sync.Pool
-}
-
-func (wp *writerPool) Get(w io.Writer) *byteio.StickyLittleEndianWriter {
-	lw := wp.pool.Get().(*byteio.StickyLittleEndianWriter)
-	lw.Writer = w
-
-	return lw
-}
-
-func (wp *writerPool) Put(lw *byteio.StickyLittleEndianWriter) (int64, error) {
-	c, err := lw.Count, lw.Err
-	*lw = byteio.StickyLittleEndianWriter{}
-
-	wp.pool.Put(lw)
-
-	return c, err
-}
-
-var (
-	aReaderPool = readerPool{
-		pool: sync.Pool{
-			New: func() interface{} {
-				return new(byteio.StickyLittleEndianReader)
-			},
-		},
-	}
-	aWriterPool = writerPool{
-		pool: sync.Pool{
-			New: func() interface{} {
-				return new(byteio.StickyLittleEndianWriter)
-			},
-		},
-	}
 )
 
 // String is a string that implements io.ReaderFrom and io.WriterTo.
@@ -71,19 +13,19 @@ type String string
 
 // ReadFrom decodes the string from the Reader.
 func (t *String) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = String(lr.ReadStringX())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadStringX()
+	*t = String(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the string to the Writer.
 func (t String) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteStringX(string(t))
 
-	lw.WriteStringX(string(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Uint8 is a uint8 that implements io.ReaderFrom and io.WriterTo.
@@ -91,19 +33,19 @@ type Uint8 uint8
 
 // ReadFrom decodes the uint8 from the Reader.
 func (t *Uint8) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Uint8(lr.ReadUint8())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadUint8()
+	*t = Uint8(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the uint8 to the Writer.
 func (t Uint8) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteUint8(uint8(t))
 
-	lw.WriteUint8(uint8(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Uint16 is a uint16 that implements io.ReaderFrom and io.WriterTo.
@@ -111,19 +53,19 @@ type Uint16 uint16
 
 // ReadFrom decodes the uint16 from the Reader.
 func (t *Uint16) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Uint16(lr.ReadUint16())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadUint16()
+	*t = Uint16(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the uint16 to the Writer.
 func (t Uint16) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteUint16(uint16(t))
 
-	lw.WriteUint16(uint16(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Uint32 is a uint32 that implements io.ReaderFrom and io.WriterTo.
@@ -131,19 +73,19 @@ type Uint32 uint32
 
 // ReadFrom decodes the uint32 from the Reader.
 func (t *Uint32) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Uint32(lr.ReadUint32())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadUint32()
+	*t = Uint32(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the uint32 to the Writer.
 func (t Uint32) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteUint32(uint32(t))
 
-	lw.WriteUint32(uint32(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Uint64 is a uint64 that implements io.ReaderFrom and io.WriterTo.
@@ -151,19 +93,19 @@ type Uint64 uint64
 
 // ReadFrom decodes the uint64 from the Reader.
 func (t *Uint64) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Uint64(lr.ReadUint64())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadUint64()
+	*t = Uint64(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the uint64 to the Writer.
 func (t Uint64) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteUint64(uint64(t))
 
-	lw.WriteUint64(uint64(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Uint is a uint that implements io.ReaderFrom and io.WriterTo.
@@ -171,19 +113,19 @@ type Uint uint
 
 // ReadFrom decodes the uint from the Reader.
 func (t *Uint) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Uint(lr.ReadUintX())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadUintX()
+	*t = Uint(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the uint to the Writer.
 func (t Uint) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteUintX(uint64(t))
 
-	lw.WriteUintX(uint64(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Int8 is a int8 that implements io.ReaderFrom and io.WriterTo.
@@ -191,19 +133,19 @@ type Int8 int8
 
 // ReadFrom decodes the int8 from the Reader.
 func (t *Int8) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Int8(lr.ReadInt8())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadInt8()
+	*t = Int8(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the int8 to the Writer.
 func (t Int8) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteInt8(int8(t))
 
-	lw.WriteInt8(int8(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Int16 is a int16 that implements io.ReaderFrom and io.WriterTo.
@@ -211,19 +153,19 @@ type Int16 int16
 
 // ReadFrom decodes the int16 from the Reader.
 func (t *Int16) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Int16(lr.ReadInt16())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadInt16()
+	*t = Int16(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the int16 to the Writer.
 func (t Int16) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteInt16(int16(t))
 
-	lw.WriteInt16(int16(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Int32 is a int32 that implements io.ReaderFrom and io.WriterTo.
@@ -231,19 +173,19 @@ type Int32 int32
 
 // ReadFrom decodes the int32 from the Reader.
 func (t *Int32) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Int32(lr.ReadInt32())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadInt32()
+	*t = Int32(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the int32 to the Writer.
 func (t Int32) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteInt32(int32(t))
 
-	lw.WriteInt32(int32(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Int64 is a int64 that implements io.ReaderFrom and io.WriterTo.
@@ -251,19 +193,19 @@ type Int64 int64
 
 // ReadFrom decodes the int64 from the Reader.
 func (t *Int64) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Int64(lr.ReadInt64())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadInt64()
+	*t = Int64(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the int64 to the Writer.
 func (t Int64) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteInt64(int64(t))
 
-	lw.WriteInt64(int64(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Int is a int that implements io.ReaderFrom and io.WriterTo.
@@ -271,19 +213,19 @@ type Int int
 
 // ReadFrom decodes the int from the Reader.
 func (t *Int) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Int(lr.ReadIntX())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadIntX()
+	*t = Int(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the int to the Writer.
 func (t Int) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteIntX(int64(t))
 
-	lw.WriteIntX(int64(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Float32 is a float32 that implements io.ReaderFrom and io.WriterTo.
@@ -291,19 +233,19 @@ type Float32 float32
 
 // ReadFrom decodes the float32 from the Reader.
 func (t *Float32) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Float32(lr.ReadFloat32())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadFloat32()
+	*t = Float32(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the float32 to the Writer.
 func (t Float32) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteFloat32(float32(t))
 
-	lw.WriteFloat32(float32(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
 
 // Float64 is a float64 that implements io.ReaderFrom and io.WriterTo.
@@ -311,17 +253,17 @@ type Float64 float64
 
 // ReadFrom decodes the float64 from the Reader.
 func (t *Float64) ReadFrom(r io.Reader) (int64, error) {
-	lr := aReaderPool.Get(r)
-	*t = Float64(lr.ReadFloat64())
+	lr := byteio.LittleEndianReader{Reader: r}
+	v, n, err := lr.ReadFloat64()
+	*t = Float64(v)
 
-	return aReaderPool.Put(lr)
+	return int64(n), err
 }
 
 // WriteTo encodes the float64 to the Writer.
 func (t Float64) WriteTo(w io.Writer) (int64, error) {
-	lw := aWriterPool.Get(w)
+	lw := byteio.LittleEndianWriter{Writer: w}
+	n, err := lw.WriteFloat64(float64(t))
 
-	lw.WriteFloat64(float64(t))
-
-	return aWriterPool.Put(lw)
+	return int64(n), err
 }
